9e47c38ccc6f00ed10c312094377c4b49029d0c9
Manuel Meurer <manuel@krautcomputing.com>
2013-11-06 10:53:00 +0100

    Use require_relative instead of require

--- lib/rails_bootstrap_navbar.rb
+++ lib/rails_bootstrap_navbar.rb
@@ -1,2 +1,2 @@
-require 'rails_bootstrap_navbar/version'
-require 'rails_bootstrap_navbar/railtie' if defined?(Rails)
+require_relative 'rails_bootstrap_navbar/version'
+require_relative 'rails_bootstrap_navbar/railtie'
